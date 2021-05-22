fe5678fb5e487689f9e6e9bb34d7ea1a7c6980da
john-rey-edralin <80669817+john-rey-edralin@users.noreply.github.com>
2021-05-22 21:19:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,11 +14,36 @@ const db = require('./models/db.js');
 
 //const bodyParser = require(`body-parser`);
 
+// import module `express-session`
+const session = require('express-session');
+
+// import module `mongoose`
+const mongoose = require('mongoose');
+
+// import module `connect-mongo`
+const MongoStore = require('connect-mongo');
+
 const app = express();
 
 dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
+
+db.connect();
+
+// use `express-session`` middleware and set its options
+// use `MongoStore` as server-side session storage
+console.log(`this is    ` + process.env.DB_URL);
+app.use(session({
+    'secret': 'disco-petitio',
+    'resave': false,
+    'saveUninitialized': false,
+    store: MongoStore.create({
+	    mongoUrl: process.env.DB_URL,
+	    ttl: 21 * 24 * 60 * 60 // = 21 days. Default
+	  })
+}));
+
 
 // set `hbs` as view engine
 app.set('view engine', 'hbs');
@@ -54,14 +79,37 @@ app.use(express.static('public'));
 // define the paths contained in `./routes/routes.js`
 app.use('/', routes);
 
+app.use(function (req, res) {
+
+    var details = {};
+
+    /*
+        checks if a user is logged-in by checking the session data
+        if a user is logged-in,
+        display the profile tab and logout tab in the nav bar.
+    */
+    if(req.session.username) {
+        details.flag = true;
+        details.name = req.session.name;
+        details.idNum = req.session.idNum;
+    }
+
+    /*
+        if no user is logged-in,
+        do not display the profile tab and the logout tab in the nav bar.
+    */
+    else
+        details.flag = false;
+
+    // render `../views/error.hbs`
+    res.render(`index`);
+});
+
 // if the route is not defined in the server, render `../views/error.hbs`
 // always define this as the last middleware
 // app.use(function (req, res) {
 //     res.render('error');
 // });
-
-// connects to the database
-db.connect();
 
 // binds the server to a specific port
 app.listen(port, function () {
